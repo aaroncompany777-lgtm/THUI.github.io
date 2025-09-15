bb1a17c4ad80696493a925537989858e09b36c65
aaroncompany777-lgtm <aaron.company777@gmail.com>
2025-09-15 22:38:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-<script>
-  // Scroll reveal effect
-  const reveals = document.querySelectorAll(".reveal");
 
-  window.addEventListener("scroll", () => {
-    for (let i = 0; i < reveals.length; i++) {
-      const windowHeight = window.innerHeight;
-      const elementTop = reveals[i].getBoundingClientRect().top;
-      const revealPoint = 100; // trigger point
-
-      if (elementTop < windowHeight - revealPoint) {
-        reveals[i].classList.add("active");
-      }
-    }
-  });
-</script>
